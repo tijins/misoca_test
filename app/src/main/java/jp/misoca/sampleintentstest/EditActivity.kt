995c966ce0993507421d7dff007c8145e11fd980
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-05-28 17:23:43 +0900

    画面遷移と共有を追加

--- app/src/main/java/jp/misoca/sampleintentstest/EditActivity.kt
+++ app/src/main/java/jp/misoca/sampleintentstest/EditActivity.kt
@@ -1,11 +1,20 @@
 package jp.misoca.sampleintentstest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import jp.misoca.sampleintentstest.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityEditBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit)
+
+
+        binding.editText.setText(intent.getStringExtra(Intent.EXTRA_TEXT))
     }
 }
--- app/src/main/java/jp/misoca/sampleintentstest/MainActivity.kt
+++ app/src/main/java/jp/misoca/sampleintentstest/MainActivity.kt
@@ -1,11 +1,40 @@
 package jp.misoca.sampleintentstest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import jp.misoca.sampleintentstest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnEdit.setOnClickListener {
+            openEditActivity()
+        }
+        binding.btnShare.setOnClickListener {
+            share()
+        }
+    }
+
+    private fun openEditActivity(){
+        val intent = Intent(this, EditActivity::class.java).apply {
+            putExtra(Intent.EXTRA_TEXT, binding.txtSubject.text.toString())
+        }
+        startActivity(intent)
+    }
+
+    private fun share(){
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, binding.txtSubject.text.toString())
+        }
+        val chooser = Intent.createChooser(intent,getString(R.string.main_share))
+        startActivity(chooser)
     }
 }
