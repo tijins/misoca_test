1eece5de0b0af46ff548ca5b3085ad4a967f6d90
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-05-28 16:28:48 +0900

    EditActivityを追加

---
+++ app/src/main/java/jp/misoca/sampleintentstest/EditActivity.kt
@@ -0,0 +1,11 @@
+package jp.misoca.sampleintentstest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+    }
+}
